4754b7979f7db9803b6ec332018efca375c36420
Eugene Lozovoy <uzix.ls@gmail.com>
2023-07-13 12:26:03 +0300

    file_get_next_byte: tiny optimization

--- src/file.asm
+++ src/file.asm
@@ -8,11 +8,8 @@
 ; 6 110             slow
 ; 7 111 altscr slow slow
 
-file_page0 equ #10
-file_page1 equ #14
-file_page2 equ #16
-file_page3 equ #13
-file_pages: db file_page0, file_page1, file_page2, file_page3
+    align 8
+file_pages: db #10, #14, #16, #13
 
 file_base_addr equ #c000
 file_page_size equ #4000
@@ -25,24 +22,22 @@ file_page_size equ #4000
 ; OUT - BC - garbage
 file_get_next_byte:
     ld a, h                          ; compare requested page with current page
-    and #c0                          ; ...
+    and #c0                          ; ... page_number = position[7:6]
 .pg:cp #0f                           ; ... self modifying code! see bellow and file_load
     jp z, .get                       ; ...
 .switch_page:
     ld (.pg+1), a                    ;
-    .4 rrca                          ; A = 0/4/8/12
-    ld (.A+1), a                     ;
-.A  jr $                             ;
-    ld a, file_page0 : jr 1f         ; 2 bytes + 2 bytes
-    ld a, file_page1 : jr 1f         ;
-    ld a, file_page2 : jr 1f         ;
-    ld a, file_page3                 ;
-1:  ld bc, #7ffd                     ;
+    ld bc, file_pages                ; A = *(file_pages + (page_number >> 6))
+    rlca : rlca                      ; ...
+    add a, c                         ; ...
+    ld c, a                          ; ...
+    ld a, (bc)                       ; ...
+    ld bc, #7ffd                     ;
     out (c), a                       ;
 .get:
     ld a, h                          ; position = position[5:0]
     and #3f                          ; ...
-    add high file_base_addr          ; A = *(base_addr + position)
+    add a, high file_base_addr       ; A = *(base_addr + position)
     ld b, a                          ; ...
     ld c, l                          ; ...
     ld a, (bc)                       ; ...
